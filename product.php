87293e1f911d51a2512ab331806143d1c0922691
veloisa <veloisafernandesco@gmail.com>
2022-11-14 13:45:27 +0530

    product form added

---
+++ product.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Users table</title>
+</head>
+<body>
+    <table border="1px" style="width:600px; line-height:40px;">
+<thead>
+<tr>
+  <th>name</th>
+  <th>date_of_purchase</th>
+  <th>branch</th>
+  <th>warranty_period</th>
+  <th>status</th>
+  <th>created_at</th>
+  <th>updated_at</th>
+</tr>
+</thead>
+<tbody>
+<?php
+$host = "localhost"; //IP of your database
+$userName = "root"; //Username for database login
+$userPass = ""; //Password associated with the username
+
+$connectQuery = mysqli_connect($host,$userName,$userPass,"stocks");
+
+
+
+if(mysqli_connect_errno()){
+    echo mysqli_connect_error();
+    exit();
+}else{
+    $selectQuery = "SELECT * FROM `product`";
+    $result = mysqli_query($connectQuery,$selectQuery);
+    if(mysqli_num_rows($result) > 0){
+        
+        while($row = mysqli_fetch_assoc($result)){ ?>
+
+            <tr>
+                    <td><?php echo $row['name']; ?></td>
+                    <td><?php echo $row['date_of_purchase']; ?></td>
+                    <td><?php echo $row['branch']; ?></td>
+                    <td><?php echo $row['warranty_period']; ?></td>
+                    <td><?php echo $row['status']; ?></td>
+                    <td><?php echo $row['created_at']; ?></td>
+                    <td><?php echo $row['updated_at']; ?></td>
+                </tr>
+            
+   <?php     }
+
+    }
+
+    
+
+}
+?>
+
+    </tbody>
+</table>
+</body>
+</html>
