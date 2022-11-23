f8f32b55b7a895f28bb61ddd4036fed8d36c37fc
veloisa <veloisafernandesco@gmail.com>
2022-11-23 21:30:45 +0530

    productupdate added

--- productupdate.php
+++ productupdate.php
@@ -1,7 +1,11 @@
 <?php
-$host = "localhost"; //IP of your database
-$userName = "root"; //Username for database login
-$userPass = ""; //Password associated with the username
+$host = "localhost";
+$userName = "root";
+$name = $_POST["name"];
+$date_of_purchase = $_POST["date_of_purchase"];
+$branch = $_POST["branch"];
+$warranty_period = $_POST["warranty_period"];
+$userPass = "";
 
 $connectQuery = mysqli_connect($host,$userName,$userPass,"stocks");
 
@@ -11,8 +15,23 @@ if(mysqli_connect_errno()){
     echo mysqli_connect_error();
     exit();
 }
-echo $_GET['id'];
-                    $sql = "UPDATE FROM product WHERE id=".$_GET['id'];
+echo $_POST['id'];
+f ($conn->connect_error){
+	die("Connection failed: ". $conn->connect_error);
+}
+
+$sql = "update product set name='$name', date of purchase='$date_of_purchase',branch ='$branch',warranty period='$warranty_period' where product='$product'";
+
+if ($conn->query($sql) === TRUE) {
+	echo "Records updated: ".$name."-".$date of purchase."-".$branch."--".$warranty period.".$gender;
+} else {
+	echo "Error: ".$sql."<br>".$conn->error;
+}
+
+$conn->close();
+
+?>                    
+$sql = "UPDATE FROM product WHERE id=".$_GET['id'];
                     if (mysqli_query($connectQuery,$sql)) {
                       echo "Record updated successfully";
                     } else {
